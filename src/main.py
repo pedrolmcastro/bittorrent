ca780e0db3909498eb37c550215a34cd5f2740ea
Pedro Lucas de Moliner de Castro <pedro.lucas.moliner.castro@gmail.com>
2023-06-27 21:31:35 -0300

    Move source files to src/

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ import bencode
 
 
 def main():
-    with open("torrent/debian-10.10.0-amd64-netinst.iso.torrent", "rb") as file:
+    with open("../torrent/debian-10.10.0-amd64-netinst.iso.torrent", "rb") as file:
         decoded = bencode.decode(file.read())
         print(decoded[b"info"][b"name"])
 
