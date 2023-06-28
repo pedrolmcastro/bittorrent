b0be1073623d12425a5422272206cc75dc803c60
Pedro Lucas de Moliner de Castro <pedro.lucas.moliner.castro@gmail.com>
2023-06-28 16:05:30 -0300

    Add peer id generator credit

--- src/peer.py
+++ src/peer.py
@@ -3,7 +3,7 @@ import string
 
 
 def gen_id():
-    # Format: -<2B client ID><4B client version>-<12B random chars>
+    # From: https://markuseliasson.se/article/bittorrent-in-python/
     CLIENT = b"PY"
     VERSION = b"0001"
     CHARS = string.ascii_letters + string.digits
